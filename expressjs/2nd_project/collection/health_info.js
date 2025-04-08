43b53b58748a7bbd261e514b701ccb421d6a6389
aiman usman <>
2025-04-08 11:10:55 +0500

    collection

---
+++ expressjs/2nd_project/collection/health_info.js
@@ -0,0 +1,26 @@
+let ikrama = require('mongoose');
+
+let health_collection = ikrama.Schema({
+    sugar: {
+        type: Number,
+        required: true
+    },
+    heart_rate : {
+        type: Number,
+        required: true
+    },
+    bp: {
+        type: Number,
+        required: true
+    },
+    colestrol: {
+        type: Number,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = ikrama.model('health_info', health_collection);
--- expressjs/2nd_project/index.js
+++ expressjs/2nd_project/index.js
@@ -3,11 +3,66 @@ let route = require('./routes/router');
 require('dotenv').config();
 let db = require('./connect_to_db/connection')
 
+// export karien gy yahan pr un file ko 
+let user = require('./collection/user_model');
+let personal = require('./collection/personal_record');
+let health = require('./collection/health_info');
+
+// data add karien gy model mai
+let add_data = async function(){
+    try {
+        user.create({
+            name: 'Rahul',
+            email: 'rahul@gmail.com',
+            password: '123456',
+            gender: 'male',
+            age: 25,
+            address: 'delhi',
+        })
+        console.log('user created successfully')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+let personal_data = async function(){
+    try {
+        personal.create({
+            height: 5.5,
+            weight: 60,
+            bmi: 25,
+            category: 'normal',
+        })
+        console.log('personal data created successfully')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+let  health_data= async function(){
+    try {
+        health.create({
+            sugar: 100,
+            bp: 120,
+            heart_rate: 80,
+            colestrol: 150,
+        })
+        console.log('health data created successfully')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
 let app = exp();
 // use routing file
 app.use('/ikrama/route',route)
 
 db().then(()=>{
+    add_data();
+    personal_data();
+    health_data();
     app.listen(process.env.PORT,()=>{
         console.log(`server is running on port http://localhost:${process.env.PORT}/ikrama/route/`);
 })
